766ba63434f89638c51a0e31806df601d6815dd0
bluethundergames <whiterabbitwebstudios@gmail.com>
2025-01-07 22:18:30 -0500

    Added Links on Introduction for Login and Git Repo

--- resources/js/Pages/Introduction.tsx
+++ resources/js/Pages/Introduction.tsx
@@ -1,5 +1,5 @@
 import Guest from '@/Layouts/GuestLayout';
-import { Head } from '@inertiajs/react';
+import { Head, Link } from '@inertiajs/react';
 
 export default function Introduction() {
     return (
@@ -56,6 +56,13 @@ export default function Introduction() {
                     <h4 className="font-bold">User</h4>
                     <p>Email: user@example.com</p>
                     <p>Password: password</p>
+                </div>
+                <div className="flex">
+                    <Link
+                        className="inline-flex items-center rounded-md border border-transparent bg-gray-800 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-gray-700 focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:bg-gray-900 dark:bg-gray-200 dark:text-gray-800 dark:hover:bg-white dark:focus:bg-white dark:focus:ring-offset-gray-800 dark:active:bg-gray-300 false"
+                        href={route('login')}
+                    >Click here to Login</Link>
+                    <a href="https://github.com/BlueThunderGames/laravel-react-ssr" className="inline-flex items-center rounded-md border border-transparent bg-gray-800 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-gray-700 focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:bg-gray-900 dark:bg-gray-200 dark:text-gray-800 dark:hover:bg-white dark:focus:bg-white dark:focus:ring-offset-gray-800 dark:active:bg-gray-300 false ms-4" target="_blank">View Code</a>
                 </div>
             </div>
         </Guest>
