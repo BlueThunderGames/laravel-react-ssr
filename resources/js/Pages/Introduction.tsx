be1afb1d614408583884ed20f6cc75e4a9f28f60
bluethundergames <whiterabbitwebstudios@gmail.com>
2025-01-09 09:11:12 -0500

    Updates to buttons and copy on Introduction Component

--- resources/js/Pages/Introduction.tsx
+++ resources/js/Pages/Introduction.tsx
@@ -12,7 +12,7 @@ export default function Introduction() {
         >
             <Head title="Introduction" />
             <div className="intro-upper mb-6">
-                <h3 className="text-lg font-bold mb-4">Welcome to my Features App powered by React, Typescript, and Laravel</h3>
+                <h3 className="text-lg font-bold mb-4">Welcome to my Features App powered by React, TypeScript, and Laravel</h3>
                 <p>This application allows users to interact with various Features, each with a title and description. Depending on your role, you have access to different functionality. Here's a breakdown of what each role can do:</p>
             </div>
             <div className="role-breakdown mb-6">
@@ -57,12 +57,12 @@ export default function Introduction() {
                     <p>Email: user@example.com</p>
                     <p>Password: password</p>
                 </div>
-                <div className="flex">
+                <div className="block sm:flex items-start">
                     <Link
-                        className="inline-flex items-center rounded-md border border-transparent bg-gray-800 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-gray-700 focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:bg-gray-900 dark:bg-gray-200 dark:text-gray-800 dark:hover:bg-white dark:focus:bg-white dark:focus:ring-offset-gray-800 dark:active:bg-gray-300 false"
+                        className="max-sm:block sm:inline-flex max-md:mb-4 text-center items-center rounded-md border border-transparent bg-gray-800 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-gray-700 focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:bg-gray-900 dark:bg-gray-200 dark:text-gray-800 dark:hover:bg-white dark:focus:bg-white dark:focus:ring-offset-gray-800 dark:active:bg-gray-300 false"
                         href={route('login')}
                     >Click here to Login</Link>
-                    <a href="https://github.com/BlueThunderGames/laravel-react-ssr" className="inline-flex items-center rounded-md border border-transparent bg-gray-800 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-gray-700 focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:bg-gray-900 dark:bg-gray-200 dark:text-gray-800 dark:hover:bg-white dark:focus:bg-white dark:focus:ring-offset-gray-800 dark:active:bg-gray-300 false ms-4" target="_blank">View Code</a>
+                    <a href="https://github.com/BlueThunderGames/laravel-react-ssr" className="max-md:block md:inline-flex text-center items-center rounded-md border border-transparent bg-gray-800 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-gray-700 focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:bg-gray-900 dark:bg-gray-200 dark:text-gray-800 dark:hover:bg-white dark:focus:bg-white dark:focus:ring-offset-gray-800 dark:active:bg-gray-300 false sm:ms-4" target="_blank">View Code</a>
                 </div>
             </div>
         </Guest>
